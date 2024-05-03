55692f36023ebb13a5fcb836601a0f0b21895d0a
Bartosz Bielecki <bartosz.bielecki@outlook.com>
2024-05-03 12:49:10 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryItems = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
 
-console.log(galleryItems);
+const galleryList = document.createElement("ul");
+galleryList.classList.add("gallery");
+document.body.appendChild(galleryList);
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.url;
+  galleryLink.target = "_blank";
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.url;
+  galleryImage.dataset.source = item.url;
+  galleryImage.alt = item.alt;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+galleryItems.forEach((item) => {
+  const galleryItem = createGalleryItem(item);
+  galleryList.appendChild(galleryItem);
+});
