f846c789ea8d357e29325189dad4a1ae2a953919
Bartosz Bielecki <bartosz.bielecki@outlook.com>
2024-05-03 12:59:26 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,4 @@
-import galleryItems from "./gallery-items.js";
+import galleryItems from "./js/gallery-items.js";
 
 const galleryList = document.createElement("ul");
 galleryList.classList.add("gallery");
